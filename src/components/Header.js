d126b63e287bfeb45fb20de6d2ceb7607fbbc28c
Venkat Arun <arunkumarpv04@gmail.com>
2025-05-14 05:20:32 -0500

    Add login-logout feature

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,10 @@
+import { useState } from "react";
 import { CDN_URL } from "../utils/constants";
 
 const Header=()=>{
+
+    let [bntName,setBntName]=useState("login");
+
     return (
         <div className="header">
             <div className="logo">
@@ -9,8 +13,15 @@ const Header=()=>{
             <div className="nav-items">
                 <ul>
                     <li>Home</li>
-                    <li>About</li>
+                    <li>About us</li>
+                    <li>Contact us</li>
                     <li>Cart</li>
+                    <button className="btn-login" onClick={
+                        ()=> {
+                            return bntName==="login" ?
+                            setBntName("logout"):setBntName("login");
+                        }
+                    }>{bntName}</button>
                 </ul>
             </div>
         </div>
---
+++ src/components/SkimmerUI.js
@@ -0,0 +1,34 @@
+const SkimmerUI=()=>
+{
+    return (
+       <div className="skim-container">
+        <div className="skim-card-container">
+
+        </div>
+        <div className="skim-card-container">
+            
+        </div>
+        <div className="skim-card-container">
+            
+        </div>
+        <div className="skim-card-container">
+            
+        </div>
+         <div className="skim-card-container">
+            
+        </div>
+         <div className="skim-card-container">
+            
+        </div>
+         <div className="skim-card-container">
+            
+        </div>
+         <div className="skim-card-container">
+            
+        </div>
+    
+       </div>
+    );
+}
+
+export default SkimmerUI;
