1dab0da19743656e28d086f77bdb81e993363e3f
Venkat Arun <arunkumarpv04@gmail.com>
2025-05-16 02:37:44 -0500

    add Search Feature

--- src/components/Body.js
+++ src/components/Body.js
@@ -5,6 +5,9 @@ import { useEffect } from "react";
 import SkimmerUI from "./SkimmerUI";
 const Body=()=>{
     let [resList,setRestoData]=useState([]);
+    const [filteredList,setFilteredList]=useState([]);
+    const [searchValue,setSearchValue]=useState("");
+
     useEffect(
         ()=>{
             fetchData();
@@ -14,15 +17,28 @@ const Body=()=>{
         const data=await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.6126255&lng=77.04108959999999&offset=${cart}&sortBy=RELEVANCE&pageType=SEE_ALL&page_type=DESKTOP_SEE_ALL_LISTING");
         const json=await data.json();
         setRestoData(json.data.cards[4].card.card.gridElements.infoWithStyle.restaurants)
+        setFilteredList(json.data.cards[4].card.card.gridElements.infoWithStyle.restaurants)
     }
 
     
              
     return resList.length===0 ? <SkimmerUI/> :(
         <div className="Body">
-            <div className="search">
+            <div className="filter">
+                <div className="search">
+                    <input type="text" value={searchValue} onChange={
+                        (e)=>setSearchValue(e.target.value)
+                    }>
+                    </input>
+                    <button onClick={
+                        ()=>{
+                            resList=filteredList.filter((restaurant)=>restaurant.info.name.toLowerCase().includes(searchValue.toLowerCase()))
+                            setRestoData(resList);
+                        }
+                    }>Search</button>
+                </div>
                 <button className="btn-rated" onClick={()=>{
-                    resList=resList.filter((restaurant)=>restaurant.info.avgRating>4.5);
+                    resList=resList.filter((restaurant)=>restaurant.info.avgRating>4.4);
                     setRestoData(resList);
                 }
                 }
